b1f74a56a57a39c37ae4971f27cbe3a05ba0b915
Karl Gemayel <karl@gatech.edu>
2019-12-18 18:31:29 -0500

    Downloading genomes from taxonomy tree

--- sbsp/code/python/driver/download-data-by-ancestor.py
+++ sbsp/code/python/driver/download-data-by-ancestor.py
@@ -6,6 +6,7 @@
 import os
 import logging
 import argparse
+from datetime import datetime
 
 from typing import *
 
@@ -26,18 +27,20 @@ from sbsp_io.general import mkdir_p
 
 parser = argparse.ArgumentParser("Description of driver.")
 
-ancestor_grp = parser.add_mutually_exclusive_group(required=True)
-ancestor_grp.add_argument('--ancestor-name', help="Name of ancestor")
-ancestor_grp.add_argument('--ancestor-tax-id', type=int, help="Ancestor taxonomy id")
+parser.add_argument("--ancestor-id", required=True, help="ID of ancestor")
+parser.add_argument("--ancestor-id-type", required=True, choices=["taxid", "name_txt"], help="Type of ID for ancestor. See choices.")
 
 parser.add_argument('--pf-taxonomy-tree', required=True, help="Pickle file containing taxonomy tree")
 parser.add_argument('--pf-assembly-summary', required=True, help="Assembly summary file")
+
+parser.add_argument('--valid-assembly-levels', choices={"Complete Genome", "Scaffold", "Contig"}, nargs="+")
+parser.add_argument('--favor-assembly-level-order', action="store_true", default=False)
+parser.add_argument('--number-per-taxid', type=int, default=None)
 
 parser.add_argument('--pd-output', required=True, help="Path to output directory where data will be downloaded")
 parser.add_argument('--pf-output-list', required=True, help="Path to list file containing genome names")
 
 parser.add_argument('--dry-run', default=False, action="store_true")
-
 
 parser.add_argument('--pd-work', required=False, default=None, help="Path to working directory")
 parser.add_argument('--pd-data', required=False, default=None, help="Path to data directory")
@@ -71,7 +74,9 @@ def set_up_gcfid(gcfid_info, pd_output):
     gcfid = "{}_{}".format(gcf, acc)
 
     pd_gcfid = os.path.join(pd_output, gcfid)
+    pd_runs = os.path.join(pd_gcfid, "runs")
     mkdir_p(pd_gcfid)
+    mkdir_p(pd_runs)
 
     ftplink = gcfid_info["ftp_path"]
     fn_sequence = "{}_genomic.fna".format(gcfid)
@@ -82,6 +87,10 @@ def set_up_gcfid(gcfid_info, pd_output):
 
     pf_local_sequence = os.path.join(pd_gcfid, "sequence.fasta")
     pf_local_labels = os.path.join(pd_gcfid, "ncbi.gff")
+
+    # don't re-download. TODO: add option to force re-download
+    if os.path.isfile(pf_local_sequence) and os.path.isfile(pf_local_labels):
+        return
 
     run_shell_cmd(
         "pwd; cd {}; wget --quiet {}; wget --quiet {}; gunzip -f {}; gunzip -f {}".format(
@@ -103,21 +112,59 @@ def set_up_gcfid(gcfid_info, pd_output):
     )
 
 
+def filter_list(list_info, **kwargs):
+    # type: (List[Dict[str, Any]], Dict[str, Any]) -> List[Dict[str, Any]]
 
+    if len(list_info) == 0:
+        return list()
 
+    possible_assembly_levels = {"Complete Genome", "Scaffold", "Contig"}
 
+    valid_assembly_levels = get_value(kwargs, "valid_assembly_levels", possible_assembly_levels, default_if_none=True)
+    favor_assembly_level_order = get_value(kwargs, "favor_assembly_level_order", False)
+    number_per_taxid = get_value(kwargs, "number_per_taxid", None)
 
+    list_info_filtered = list()
+
+    def select_from_list(local_list_info, n):
+        # type: (List[Dict[str, Any]], Union[None, int]) -> List[Dict[str, Any]]
+
+        if n is None:
+            return local_list_info
+
+        if len(local_list_info) <= n:
+            return local_list_info
+
+        return local_list_info[0:n]
+
+    list_info = sorted(list_info, reverse=True, key=lambda x: datetime.strptime(x["seq_rel_date"], "%Y/%m/%d"))
+
+    if favor_assembly_level_order:
+
+        for assembly_level in valid_assembly_levels:
+
+            list_info_filtered += select_from_list(
+                [x for x in list_info if x["assembly_level"] == assembly_level],
+                number_per_taxid - len(list_info_filtered)
+            )
+
+            if len(list_info_filtered) == number_per_taxid:
+                break
+    else:
+        list_info_filtered += select_from_list(list_info, number_per_taxid)
+
+    return list_info_filtered
 
 
 def download_data_by_ancestor(env, ancestor_tag, tag_type, pf_taxonomy_tree, pf_assembly_summary, pd_output,
                               pf_output_list, **kwargs):
     # type: (Environment, Union[str, int], str, str, str, str, str, Dict[str, Any]) -> None
 
+    pd_output = os.path.abspath(pd_output)
+
     tax_tree = TaxonomyTree.load(pf_taxonomy_tree)
 
     taxid_to_info_list = get_rows_by_key(pf_assembly_summary, key="taxid")
-    counter = 0
-    total = 0
 
     dry_run = get_value(kwargs, "dry_run", False)
 
@@ -131,7 +178,7 @@ def download_data_by_ancestor(env, ancestor_tag, tag_type, pf_taxonomy_tree, pf_
         if tax_id in taxid_to_info_list:
             info_list = taxid_to_info_list[tax_id]
 
-            for gcfid_info in info_list:
+            for gcfid_info in filter_list(info_list, **kwargs):
                 if dry_run:
                     counter += 1
                     continue
@@ -151,14 +198,22 @@ def download_data_by_ancestor(env, ancestor_tag, tag_type, pf_taxonomy_tree, pf_
         gil.to_file(pf_output_list)
 
 
-
 def main(env, args):
     # type: (Environment, argparse.Namespace) -> None
 
-    pd_output = os.path.abspath(args.pd_output)
-
-    download_data_by_ancestor(env, args.ancestor_tax_id, "taxid", args.pf_taxonomy_tree, args.pf_assembly_summary,
-                              pd_output, args.pf_output_list, dry_run=args.dry_run)
+    download_data_by_ancestor(
+        env,
+        args.ancestor_id,
+        args.ancestor_id_type,
+        args.pf_taxonomy_tree,
+        args.pf_assembly_summary,
+        args.pd_output,
+        args.pf_output_list,
+        dry_run=args.dry_run,
+        valid_assembly_levels=args.valid_assembly_levels,
+        favor_assembly_level_order=args.favor_assembly_level_order,
+        number_per_taxid=args.number_per_taxid  
+    )
 
 
 
