c98e8338fe57e9ad8480741291b57b04bc990901
Karl Gemayel <karl@gatech.edu>
2019-12-18 14:15:54 -0500

    Add Anaconda environment for sbsp

---
+++ sbsp/install/create_anaconda_environment.sh
@@ -0,0 +1,8 @@
+
+# packages
+conda install -c conda-forge biopython
+conda install pandas
+conda install -c anaconda pyyaml
+conda install -c conda-forge cloudpickle
+conda install -c conda-forge matplotlib 
+conda install seaborn
