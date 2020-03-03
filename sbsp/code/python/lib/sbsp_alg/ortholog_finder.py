fad9dec567467eb2bb2af51609842570ebc587a6
Karl Gemayel <karl@gatech.edu>
2020-03-02 22:20:18 -0500

    fix

--- sbsp/code/python/lib/sbsp_alg/ortholog_finder.py
+++ sbsp/code/python/lib/sbsp_alg/ortholog_finder.py
@@ -159,8 +159,8 @@ def map_aligned_aa_to_aligned_nt(q_aligned_seq_aa, original_q_nt, q_start_aa, q_
         ))
 
     output = Seq("")
-    pos_nt_no_gaps = offset_nt * 3 +  q_start_aa * 3
-    max_pos_nt_no_gaps = offset_nt * 3 + (q_end_aa + 1) * 3
+    pos_nt_no_gaps = offset_nt +  q_start_aa * 3
+    max_pos_nt_no_gaps = offset_nt + (q_end_aa + 1) * 3
 
     for pos_aa in range(len(q_aligned_seq_aa)):
         curr_aa = q_aligned_seq_aa[pos_aa]
